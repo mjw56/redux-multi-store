217406827514f3eebda9103e326718c7b8d7794f
Mike Wilcox <mwilcox56@gmail.com>
2016-04-07 22:31:12 -0400

    import component

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
-import App from './App'
+import { App } from './App'
 import { store } from '../src'
 
 render(
