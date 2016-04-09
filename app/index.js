50f55d3d3f938936c10f3f96580a1af60dbaba03
Mike Wilcox <mwilcox56@gmail.com>
2016-04-09 16:55:50 -0400

    just render

--- app/index.js
+++ app/index.js
@@ -1,12 +1,5 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
+import React from 'react'
 import { render } from 'react-dom'
-import { App } from './App'
-import { store } from '../src'
+import { Root } from './containers/Root'
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+render(<Root />, document.getElementById('root'))
