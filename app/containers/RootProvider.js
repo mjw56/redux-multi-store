1940a49aff00b41e1766ab270ec1c59aff341b76
Mike Wilcox <mwilcox56@gmail.com>
2016-04-09 17:28:12 -0400

    rename to user `Provider`

--- app/containers/RootProvider.js
+++ app/containers/RootProvider.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { Sidebar } from './Sidebar'
+import { Sidebar } from './'
 import * as stores from '../stores'
 
 export const Root = () => (
